d486d3f1cbf1d54f2472444bc070bcedbf4b92c3
Alexandre Leote <alexandreleoteweb@gmail.com>
2024-12-12 16:53:58 +0100

    📦 NEW: Début de l'exercice Forum -> Affichage des listes category et topics

---
+++ Database/forum_alexandre.sql
@@ -0,0 +1,91 @@
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           8.0.30 - MySQL Community Server - GPL
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Listage de la structure de la base pour forum_alexandre
+CREATE DATABASE IF NOT EXISTS `forum_alexandre` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `forum_alexandre`;
+
+-- Listage de la structure de table forum_alexandre. category
+CREATE TABLE IF NOT EXISTS `category` (
+  `id_category` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  PRIMARY KEY (`id_category`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+-- Listage des données de la table forum_alexandre.category : ~5 rows (environ)
+DELETE FROM `category`;
+INSERT INTO `category` (`id_category`, `name`) VALUES
+	(1, 'HTML'),
+	(2, 'CSS'),
+	(3, 'JAVASCRIPT'),
+	(4, 'Python'),
+	(5, 'Swift');
+
+-- Listage de la structure de table forum_alexandre. user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `nickName` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `subscriptionDate` date NOT NULL,
+  `userRole` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Listage des données de la table forum_alexandre.user : ~0 rows (environ)
+DELETE FROM `user`;
+
+-- Listage de la structure de table forum_alexandre. message
+CREATE TABLE IF NOT EXISTS `message` (
+  `id_message` int NOT NULL AUTO_INCREMENT,
+  `content` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `creationDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int NOT NULL DEFAULT '0',
+  `topic_id` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_message`),
+  KEY `FK_message_user` (`user_id`),
+  KEY `FK_message_topic` (`topic_id`),
+  CONSTRAINT `FK_message_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`),
+  CONSTRAINT `FK_message_topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id_topic`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Listage des données de la table forum_alexandre.message : ~0 rows (environ)
+DELETE FROM `message`;
+
+-- Listage de la structure de table forum_alexandre. topic
+CREATE TABLE IF NOT EXISTS `topic` (
+  `id_topic` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `creationDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `isLocked` tinyint(1) NOT NULL DEFAULT '0',
+  `user_id` int NOT NULL,
+  `category_id` int NOT NULL,
+  PRIMARY KEY (`id_topic`),
+  KEY `FK_topic_user` (`user_id`),
+  KEY `FK_topic_category` (`category_id`),
+  CONSTRAINT `FK_topic_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id_category`),
+  CONSTRAINT `FK_topic_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Listage des données de la table forum_alexandre.topic : ~0 rows (environ)
+DELETE FROM `topic`;
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
---
+++ view/forum/listTopics.php
@@ -0,0 +1,11 @@
+<?php
+    $category = $result["data"]['category']; 
+    $topics = $result["data"]['topics']; 
+?>
+
+<h1>Liste des topics</h1>
+
+<?php
+foreach($topics as $topic ){ ?>
+    <p><a href="#index.php?ctrl=forum&action=discussionByTopic&id=<?= $topic->getId()?>"><?= $topic ?></a> par <?= $topic->getUser() ?></p>
+<?php }
