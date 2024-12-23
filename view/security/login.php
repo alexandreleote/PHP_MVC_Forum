e9e4002fdf06bd81173a244cecef06fba4466828
Alexandre Leote <alexandreleoteweb@gmail.com>
2024-12-23 09:46:00 +0100

    👌 IMPROVE: MAJ CSS + Login & Register Pages

--- view/security/login.php
+++ view/security/login.php
@@ -1,20 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Se connecter</h1>
-    <form action="index.php?ctrl=security&action=login" method="post">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email"><br>
+<body class="login-page no-navbar no-footer">
+    <a href="index.php" class="back-action"><i class="fa-solid fa-chevron-left"></i> Retour</a>
+    
+    <section class="container-login">
+        <article class="container-form-login">
+            <h2>Se connecter</h2>
+            <form action="index.php?ctrl=security&action=login" method="post">
+                
+                <div class="form-header">
+                    <input type="email" name="email" id="email" placeholder="Identifiant" required>
 
-        <label for="password">Mot de passe</label>
-        <input type="password" name="password" id="password"><br>
+                    <input type="password" name="password" id="password" placeholder="Mot de passe" required>
+                    <p><a href="index.php?ctrl=security&action=forgotPassword">Mot de passe oublié ?</a></p>
+                </div>
+                
+                <div class="remember">
+                    <input type="checkbox" name="remember" id="remember">
+                    <label for="remember">Se souvenir de moi</label><br>
+                </div>
+                
+                <div class="confirm">
+                    <button type="submit" name="submit" class="btn-login">Se connecter</button>
+                </div>
+            </form>
 
-        <input type="submit" name="submit" value="Se connecter">
-    </form>
+            <div class="register-link">
+                <p>Pas encore inscrit ?</p>
+                <p><a href="index.php?ctrl=security&action=register">Créer un compte</a></p>
+            </div>
+
+        </article>
+    </section>
 </body>
-</html>
--- view/security/register.php
+++ view/security/register.php
@@ -1,27 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 
-    <h1>S'inscrire</h1>
-    <form action="index.php?ctrl=security&action=register" method="post">
-        <label for="nickName">Pseudonyme</label>
-        <input type="text" name="nickName" id="nickName"><br>
+<body class="register-page no-navbar no-footer">
+    <a href="index.php" class="back-action"><i class="fa-solid fa-chevron-left"></i> Retour</a>
+    
+    <section class="container-register">
+        <article class="container-form-register">
+            <h2>S'inscrire</h2>
+            <form action="index.php?ctrl=security&action=register" method="post">
+                
+                <div class ="form-header">
+                    <input type="text" name="nickName" id="nickName" placeholder="Pseudonyme" required>
 
-        <label for="email">Mail</label>
-        <input type="email" name="email" id="email"><br>
+                    <input type="email" name="email" id="email" placeholder="Email" required>
 
-        <label for="pass1">Mot de passe</label>
-        <input type="password" name="pass1" id="pass1"><br>
-        
-        <label for="pass2">Confirmation du mot de passe</label>
-        <input type="password" name="pass2" id="pass2"><br>
+                    <input type="password" name="pass1" id="pass1" placeholder="Mot de passe" required>
+                    
+                    <input type="password" name="pass2" id="pass2" placeholder="Confirmer le mot de passe" required>
+                </div>
 
-        <input type="submit" name="submit" value="S'enregistrer">
-    </form>
+                <div class="accept-terms">
+                    <input type="checkbox" name="accept-terms" id="accept-terms" required>
+                    <label for="accept-terms">
+                        J’ai lu et j’accepte les <a href="#">Conditions d’utilisation</a> et la <a href="#">Politique de confidentialité</a> 
+                        ainsi que j’ai été informé(e) de mon droit à l’information
+                    </label>
+                </div>
+
+                <div class="confirm">
+                    <button type="submit" name="submit" class="btn-register">S'inscrire</button>
+                </div>
+
+                <div class="login-link">
+                    <p>Déjà inscrit ?</p>
+                    <p><a href="index.php?ctrl=security&action=login">Se connecter</a></p>
+                </div>
+            </form>
+        </article>
+    </section>
 </body>
-</html>
+
